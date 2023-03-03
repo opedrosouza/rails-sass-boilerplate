5e19aa9e1a633f806a0b02b8d06b3213ea3bb2ee
Pedro Souza <opedrosouza@hotmail.com>
2023-03-03 16:47:26 -0300

    removing stimulus testing controllers and generating the schema

--- app/javascript/controllers/testing_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-// Connects to data-controller="testing"
-export default class extends Controller {
-  connect() {
-    console.log("testing")
-  }
-}
