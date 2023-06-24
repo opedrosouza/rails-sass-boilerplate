15ff35e9c0b7ec3222b4a658f7fa9bca51e6cdee
Pedro Souza <opedrosouza@hotmail.com>
2023-06-23 21:33:30 -0300

    remove invalid route

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
-  namespace :storefront do
-    get 'home/index'
-  end
   devise_for :users,
              path: "auth",
              class_name: "User",
