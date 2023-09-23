58b8ef7aa9747a088645ae8891b2dc5c1df77088
Pedro Souza <opedrosouza@hotmail.com>
2023-09-22 21:27:21 -0300

    renamed javascript folder to frontend and improved forms

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import {defineConfig} from "vite"
-import FullReload from "vite-plugin-full-reload"
-import RubyPlugin from "vite-plugin-ruby"
-import StimulusHMR from "vite-plugin-stimulus-hmr"
+import {defineConfig} from "vite";
+import FullReload from "vite-plugin-full-reload";
+import RubyPlugin from "vite-plugin-ruby";
+import StimulusHMR from "vite-plugin-stimulus-hmr";
 
 export default defineConfig({
   clearScreen: false,
